934d2a4095ced9832788b7822bde5fff71cf739d
Roundand <francis.norton@gmail.com>
2016-06-19 22:37:54 +0100

    tidied up and added benchmark timing as comment.

--- bob/bob.go
+++ bob/bob.go
@@ -1,34 +1,37 @@
 // package bob responds to conversational inputs likes a grumpy teenager.
-package bob // package name must match the package name in bob_test.go
+package bob
 
-import(
-  "regexp"
-  "strings"
+import (
+	"regexp"
+	"strings"
 )
 
 const testVersion = 2 // same as targetTestVersion
 
+/////////
+//
+// this version runs "go test - bench ." in 1.637 seconds on my laptop
+//
+/////////
+
 // bob's sole function is to respond to Alice's inputs
 func Hey(in string) string {
-  hasLetters, _ := regexp.MatchString(`\pL`, in) // unicode letter character class
-  hasLetters, _ := regexp.MatchString(`\pL`, in) // unicode letter character class
-  hasDigits, _ := regexp.MatchString(`\pN`, in) // unicode number character class
-  isQuestion, _ := regexp.MatchString(`\?\s*$`, in)
-  // He answers 'Whoa, chill out!' if you yell at him.
-  if (hasLetters && in == strings.ToUpper(in)) {
-    return "Whoa, chill out!"
-  }
 
-  // Bob answers 'Sure.' if you ask him a question.
-  if (isQuestion) {
-    return "Sure."
-  }
+	// He answers 'Whoa, chill out!' if you yell at him.
+	if in != strings.ToLower(in) && in == strings.ToUpper(in) {
+		return "Whoa, chill out!"
+	}
 
-  // He says 'Fine. Be that way!' if you address him without actually saying anything.
-  if !(hasLetters || hasDigits) {
-    return "Fine. Be that way!"
-  }
+	// Bob answers 'Sure.' if you ask him a question.
+	if isQuestion, _ := regexp.MatchString(`\?\s*$`, in); isQuestion {
+		return "Sure."
+	}
 
-  // He answers 'Whatever.' to anything else.
-  return "Whatever."
+	// He says 'Fine. Be that way!' if you address him without actually saying anything.
+	if hasWord, _ := regexp.MatchString(`\w`, in); !hasWord {
+		return "Fine. Be that way!"
+	}
+
+	// He answers 'Whatever.' to anything else.
+	return "Whatever."
 }
