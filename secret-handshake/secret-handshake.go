c66bc63b48c7030edad5af64a9da9d99314dc279
Roundand <francis.norton@gmail.com>
2016-06-28 06:07:02 +0100

    re-coded as DRYer but slower

--- secret-handshake/secret-handshake.go
+++ secret-handshake/secret-handshake.go
@@ -1,34 +1,45 @@
-// secret converts a number to a sequence of body actions.
+// Package secret encodes a number as a sequence of body actions.
 package secret
 
-// Handshake converts a number to an array of strings
+// Handshake converts a number to an array of strings.
 func Handshake(secret int) []string {
+
 	var signal []string // declare and initialise signal as a slice of strings
 
+	// wrap builtin append() in a regular function for ease of reference
+	var appender = func(ss []string, s string) []string {
+		return append(ss, s)
+	}
+
+	// Reverse the order of the operations in the secret handshake
+	var reverser = func(ss []string, _ string) []string {
+		for i, l := 0, len(signal)-1; i < ((l + 1) / 2); i++ {
+			ss[i], ss[l-i] = ss[l-i], ss[i]
+		}
+		return ss
+	}
+
+	// The program should consider strings specifying an invalid binary as the value 0.
 	if secret < 0 {
 		return signal
 	}
 
-	if secret&(1<<0) != 0 { // 1 = wink
-		signal = append(signal, "wink")
+	// here's how we encode each bit of a valid binary as an action
+	var steps = []struct {
+		bit    int
+		encode func([]string, string) []string
+		action string
+	}{
+		{1 << 0, appender, "wink"},
+		{1 << 1, appender, "double blink"},
+		{1 << 2, appender, "close your eyes"},
+		{1 << 3, appender, "jump"},
+		{1 << 4, reverser, ""},
 	}
 
-	if secret&(1<<1) != 0 { // 10 = double blink
-		signal = append(signal, "double blink")
-	}
-
-	if secret&(1<<2) != 0 { // 100 = close your eyes
-		signal = append(signal, "close your eyes")
-	}
-
-	if secret&(1<<3) != 0 { // 1000 = jump
-		signal = append(signal, "jump")
-	}
-
-	if secret&(1<<4) != 0 { // 10000 = Reverse the order of the operations in the secret handshake.
-		//		l := len(signal) - 1
-		for i, l := 0, len(signal)-1; i < ((l + 1) / 2); i++ {
-			signal[i], signal[l-i] = signal[l-i], signal[i]
+	for _, step := range steps {
+		if (secret & step.bit) != 0 {
+			signal = step.encode(signal, step.action)
 		}
 	}
 
