1b267851cd997fa028e88e14e03d27e2d417fd23
Roundand <francis.norton@gmail.com>
2016-07-06 23:36:57 +0100

    First steps to algorithmic version.

--- food-chain/food-chain.go
+++ food-chain/food-chain.go
@@ -2,73 +2,87 @@
 package foodchain
 
 import (
-  "strings"
+//  "strings"
+//  "fmt"
 )
 
 const testVersion = 2
 
-var verses = []string{``,
-
-	`I know an old lady who swallowed a fly.
+var vv = []struct{text, repeats string} {
+	{`I know an old lady who swallowed a fly.
 I don't know why she swallowed the fly. Perhaps she'll die.`,
+``},
 
-	`I know an old lady who swallowed a spider.
-It wriggled and jiggled and tickled inside her.
+	{`I know an old lady who swallowed a spider.
+It wriggled and jiggled and tickled inside her.`,
+`She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.`},
+
+	{`I know an old lady who swallowed a bird.
+How absurd to swallow a bird!`,
+`She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
 She swallowed the spider to catch the fly.
-I don't know why she swallowed the fly. Perhaps she'll die.`,
+I don't know why she swallowed the fly. Perhaps she'll die.`},
 
-	`I know an old lady who swallowed a bird.
-How absurd to swallow a bird!
+	{`I know an old lady who swallowed a cat.
+Imagine that, to swallow a cat!`,
+`She swallowed the cat to catch the bird.
 She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
 She swallowed the spider to catch the fly.
-I don't know why she swallowed the fly. Perhaps she'll die.`,
+I don't know why she swallowed the fly. Perhaps she'll die.`},
 
-	`I know an old lady who swallowed a cat.
-Imagine that, to swallow a cat!
+	{`I know an old lady who swallowed a dog.
+What a hog, to swallow a dog!`,
+`She swallowed the dog to catch the cat.
 She swallowed the cat to catch the bird.
 She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
 She swallowed the spider to catch the fly.
-I don't know why she swallowed the fly. Perhaps she'll die.`,
+I don't know why she swallowed the fly. Perhaps she'll die.`},
 
-	`I know an old lady who swallowed a dog.
-What a hog, to swallow a dog!
+	{`I know an old lady who swallowed a goat.
+Just opened her throat and swallowed a goat!`,
+`She swallowed the goat to catch the dog.
 She swallowed the dog to catch the cat.
 She swallowed the cat to catch the bird.
 She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
 She swallowed the spider to catch the fly.
-I don't know why she swallowed the fly. Perhaps she'll die.`,
+I don't know why she swallowed the fly. Perhaps she'll die.`},
 
-	`I know an old lady who swallowed a goat.
-Just opened her throat and swallowed a goat!
+	{`I know an old lady who swallowed a cow.
+I don't know how she swallowed a cow!`,
+`She swallowed the cow to catch the goat.
 She swallowed the goat to catch the dog.
 She swallowed the dog to catch the cat.
 She swallowed the cat to catch the bird.
 She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
 She swallowed the spider to catch the fly.
-I don't know why she swallowed the fly. Perhaps she'll die.`,
+I don't know why she swallowed the fly. Perhaps she'll die.`},
 
-	`I know an old lady who swallowed a cow.
-I don't know how she swallowed a cow!
-She swallowed the cow to catch the goat.
-She swallowed the goat to catch the dog.
-She swallowed the dog to catch the cat.
-She swallowed the cat to catch the bird.
-She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
-She swallowed the spider to catch the fly.
-I don't know why she swallowed the fly. Perhaps she'll die.`,
-
-	`I know an old lady who swallowed a horse.
-She's dead, of course!`,
+	{`I know an old lady who swallowed a horse.
+She's dead, of course!`,``},
 }
 
+
+// returns 1-based verse of song
 func Verse(v int) string {
-  return verses[v]
+  if vv[v - 1].repeats == `` {
+    return vv[v - 1].text
+  }
+  return vv[v - 1].text + "\n" + vv[v - 1].repeats
 }
 
+// returns 1-based verses of song
 func Verses(i, j int) string {
-  return strings.Join(verses[i: j+1], "\n\n")
+  r := ""
+  for x := i; x <= j; x++ {
+    if (r != "") {
+      r += "\n\n"
+    }
+    r += Verse(x)
+  }
+  return  r
 }
 
 func Song() string {
-  return strings.Join(verses[1:], "\n\n")
+  return Verses(1, 8)
 }
