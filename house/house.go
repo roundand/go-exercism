992a98858e673a649c6c7540dc3e2582f7a4b846
Roundand <francis.norton@gmail.com>
2016-07-26 05:43:40 +0100

    Passes Song test using simple recursion.

--- house/house.go
+++ house/house.go
@@ -18,26 +18,21 @@ func EmbedSep(relPhrase, nounPhrase, sep string) string {
 // Verse generates a verse of a song with relative clauses that have
 // a recursive structure, as one long line.
 func Verse(subject string, relClauses []string, nounPhrase string) string {
-	return verseSep(subject, relClauses, nounPhrase, " ")
-}
-
-// verseSep generates a verse of a song with relative clauses that have
-// a recursive structure, using a parameterised line seperator.
-func verseSep(subject string, relClauses []string, nounPhrase, sep string) string {
+	sep := " "
 
 	// if we have a nounPhrase, add that to the rest of the verse
 	if nounPhrase != "" {
-		return EmbedSep(verseSep(subject, relClauses, "", sep), nounPhrase, sep)
+		return EmbedSep(Verse(subject, relClauses, ""), nounPhrase, sep)
 	}
 
 	// if no nounPhrase but we have a subject, add rest of verse to subject
 	if subject != "" {
-		return joinStringsSep(subject, verseSep("", relClauses, "", sep), sep)
+		return joinStringsSep(subject, Verse("", relClauses, ""), sep)
 	}
 
 	// if no nounPhrase or subject but we have multiple relClauses, add first to rest
 	if len(relClauses) > 1 {
-		return joinStringsSep(relClauses[0], verseSep("", relClauses[1:], "", sep), sep)
+		return joinStringsSep(relClauses[0], Verse("", relClauses[1:], ""), sep)
 	}
 
 	// if just one relClauses, return it
@@ -69,6 +64,14 @@ that ate the malt
 that lay in the house that Jack built.`
 */
 var nounPhrases = []NounPhrase {
+	{"the horse and the hound and the horn", "belonged to"},
+	{"the farmer sowing his corn", "kept"},
+	{"the rooster that crowed in the morn", "woke"},
+	{"the priest all shaven and shorn", "married"},
+	{"the man all tattered and torn", "kissed"},
+	{"the maiden all forlorn", "milked"},
+	{"the cow with the crumpled horn", "tossed"},
+	{"the dog", "worried"},
 	{"the cat", "killed"},
 	{"the rat", "ate"},
 	{"the malt", "lay in"},
@@ -83,25 +86,63 @@ var (
 // Song generates all verses of a recursively structured song.
 func Song() string {
 
-	var phrases []string = make([]string, len(nounPhrases))
-	for i, np := range(nounPhrases) {
-		phrases[i] = np.noun + " " + that + " " + np.phrase
-	}
-
-	song := genSong(nounPhrases, phrases)
+	song := genSong(nounPhrases)
 	fmt.Printf("song: %v\n", song)
 	return song
 }
 
-func genSong(nounPhrases []NounPhrase, phrases []string) string {
+/*
+three cases:
+
+#lastline - intro + noun
+
+#penultimate - intro + noun + that + #lastline
+
+#otherline - intro + noun + that + "\n" + rest
+*/
+func genSong(nounPhrases []NounPhrase) string {
 
 	// if this is the last entry, generate last verse and be done.
 	if (len(nounPhrases) == 1) {
-		return verseSep(joinPhrases(intro, nounPhrases[0].noun), phrases[0:0], "", "\n")
+		return joinPhrases(intro, nounPhrases[0].noun)
 	}
 
 	// generate remainder of song for remaining entries then generate verse for current entry
-	return genSong(nounPhrases[1:], phrases[1:]) + "\n\n" + verseSep(intro, phrases, "", "\n")
+	return genSong(nounPhrases[1:]) + "\n\n" + genVerse(nounPhrases)
+}
+
+/*
+three cases:
+
+#lastline - intro + noun
+
+#penultimate - intro + noun + that + #lastline
+
+#otherline - intro + noun + that + "\n" + rest
+*/
+func genVerse(nounPhrases []NounPhrase) string {
+	if len(nounPhrases) == 1 {
+		return joinPhrases(intro, nounPhrases[0].noun)
+	}
+
+	return joinPhrases(intro, nounPhrases[0].noun) + "\n" + genLines(nounPhrases)
+}
+
+func genLines(nounPhrases []NounPhrase) string {
+	if len(nounPhrases) == 0 {
+		return ""
+	}
+
+	if len(nounPhrases) == 1 {
+		return nounPhrases[0].noun
+	}
+
+	if len(nounPhrases) == 2 {
+		return that + " " + nounPhrases[0].phrase + " " + genLines(nounPhrases[1:])
+	}
+
+	return that + " " + nounPhrases[0].phrase + " " + nounPhrases[1].noun + "\n" + genLines(nounPhrases[1:])
+
 }
 
 // joinPhrases joins two strings, putting a space between them
