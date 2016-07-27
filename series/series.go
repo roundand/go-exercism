cf003b5091bf26da165634bbc2e18d250f517eef
Roundand <francis.norton@gmail.com>
2016-07-27 15:03:36 +0100

    Tidied up comments for functions

--- series/series.go
+++ series/series.go
@@ -1,7 +1,7 @@
 // Package Slice provides functions to report series of characters from strings
 package slice
 
-// function All returns an array of substrings of specified length from a string
+// All returns an array of substrings of specified length from a string
 func All(n int, s string) []string {
 	c := 1 + len(s) - n      // how many substrings will we generate?
 	r := make([]string, c)   // allocate our result slice
@@ -11,12 +11,12 @@ func All(n int, s string) []string {
 	return r
 }
 
-// function UnsafeFirst returns firsts substring of specified length from a string
+// UnsafeFirst returns firsts substring of specified length from a string
 func UnsafeFirst(n int, s string) string {
 	return s[:n] // fingers in ears, eyes closed, "la la la la la ..."
 }
 
-// function First returns first substring of specified length from a string, plus an error indicator
+// First returns first substring of specified length from a string, plus an error indicator
 func First(n int, s string) (series string, ok bool) {
 	if n > len(s) {
 		return "", false // not possible
