e0d200a38281a2cf65c4ef3375c338a7a594fc86
Roundand <francis.norton@gmail.com>
2016-07-30 17:25:50 +0100

    Solution for Exercism scrabble-score exercise.

---
+++ scrabble-score/scrabble-score.go
@@ -0,0 +1,31 @@
+// Package scrabble provides a function for scoring Scrabble words.
+package scrabble
+
+import (
+	"strings"
+)
+
+const testVersion = 4
+
+// Score generates the points cored for a scrabble word
+func Score(word string) int {
+
+	// letter points (could probably be represented more compactly and faster
+	// as []uint but wouldn't be UTF-8 safe)
+	lp := map[rune]int{
+		'a': 1, 'e': 1, 'i': 1, 'o': 1, 'u': 1, 'l': 1, 'n': 1, 'r': 1, 's': 1, 't': 1,
+		'd': 2, 'g': 1,
+		'b': 3, 'c': 3, 'm': 3, 'p': 3,
+		'f': 4, 'h': 4, 'v': 4, 'w': 4, 'y': 4,
+		'k': 5,
+		'j': 8, 'x': 8,
+		'q': 10, 'z': 10,
+	}
+
+  score := 0
+  for _, l := range strings.ToLower(word) {
+    score += lp[l]
+  }
+
+  return score
+}
