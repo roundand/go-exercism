07dd4c8f0cfb5d58af57cd36491767ed3e0960fb
Roundand <francis.norton@gmail.com>
2016-08-24 07:02:05 +0100

    Refactored to remove unnecessary step of creating a normalised rectangle, and to clarify the encryption loop logic.

--- crypto-square/crypto-square.go
+++ crypto-square/crypto-square.go
@@ -12,40 +12,39 @@ const testVersion = 2
 // Encode returns the square code encrypted version of a plaintext parameter.
 func Encode(pt string) string {
 
-	// normalised text is stripped of punctuation and forced to lower-case
+	nt := normalise(pt)
+
+	// calculate row and column lengths
+	r, c := calcSides(len(nt))
+
+	// create encrypted rectangle
+	// reading normalised rectangle row by row
+	// and writing to encrypted rectangle col by col
+	var enc []string = make([]string, c) // encrypted output
+	var cur int = 0                      // current position
+	for row := 0; row < r; row++ {       // which row of the (virtual) rectangle are we reading?
+		for col := 0; col < c; col++ {     // which column are we reading?
+			if cur >= len(nt) {
+				break
+			}
+			enc[col] += nt[cur : cur+1]      // write current character, using column as row
+			cur++
+		}
+	}
+
+	// return
+	return strings.Join(enc, " ")
+}
+
+// normalised text is stripped of punctuation and forced to lower-case
+func normalise(pt string) string {
 	nt := ""
 	for _, rn := range pt {
 		if unicode.IsLetter(rn) || unicode.IsDigit(rn) {
 			nt += strings.ToLower(string(rn))
 		}
 	}
-
-	// calculate row and column lengths
-	r, c := calcSides(len(nt))
-
-	// create normalised rectangle
-	var nr []string
-	for i := 0; i < r; i++ {
-		start := i * c
-		// last row could be short
-		end := int(math.Min(float64(start+c), float64(len(nt))))
-		nr = append(nr, nt[start:end])
-	}
-
-	// create encrypted rectangle
-	// reading normalised rectangle column by column
-	// and writing to encrypted rectangle row by row
-	var er []string = make([]string, c)
-	for ci := 0; ci < c; ci++ {
-		for ri := 0; ri < r; ri++ {
-			if len(nr[ri]) > ci {
-				er[ci] += nr[ri][ci : ci+1]
-			}
-		}
-	}
-
-	// return
-	return strings.Join(er, " ")
+  return nt
 }
 
 // calcSides calculates the squarest rectangle that will contain the message.
