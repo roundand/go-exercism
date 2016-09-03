ab37b1b00d61caeb4c12d82121e15c5627537e74
Roundand <francis.norton@gmail.com>
2016-09-03 10:14:07 +0100

    Made logic a little clearer and faster.

--- anagram/anagram.go
+++ anagram/anagram.go
@@ -13,7 +13,7 @@ func Normalise(s string) string {
 	return strings.Join(ss, "")
 }
 
-// Detect returns a list of anagrams of a specified word, filtered from a supplied list of candidates.
+// Detect returns a list of lower-case anagrams of a specified word, filtered from a supplied list of candidates.
 func Detect(word string, candidates []string) []string {
 	var anagrams []string
 	lWord := strings.ToLower(word)
@@ -21,11 +21,13 @@ func Detect(word string, candidates []string) []string {
 
 	for _, candidate := range candidates {
 		lCandidate := strings.ToLower(candidate)
-		switch nCandidate := Normalise(lCandidate); {
-		case lCandidate == lWord: // a word is not an anagram of itself
-			continue
-		case nWord == nCandidate: // we have a match
-			anagrams = append(anagrams, lCandidate)
+		switch {
+			case len(word) != len(candidate): // anagrams must be same length
+				continue
+			case lWord == lCandidate: // a word is not an anagram of itself
+				continue
+			case nWord == Normalise(lCandidate): // we have a match
+				anagrams = append(anagrams, lCandidate)
 		}
 	}
 	return anagrams
