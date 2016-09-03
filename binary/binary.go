2cf22ff122b09b7dd2e2233770104c0565d273be
Roundand <francis.norton@gmail.com>
2016-09-03 15:07:10 +0100

    Solution for Exercism Binary Parser challenge.

---
+++ binary/binary.go
@@ -0,0 +1,28 @@
+// Package binary provides a function to generate the numeric value of a binary.
+package binary
+
+import (
+	"fmt"
+)
+
+const testVersion = 1
+
+// ParseBinary parses a string of '1' and '0' values to generate the int result, or an error.
+func ParseBinary(binary string) (int, error) {
+
+	var result = 0
+	l := len(binary) - 1
+	for i, c := range binary {
+		switch c {
+		case '1':
+      fmt.Printf("binary: %s, l: %d; result: %d, i: %d, c: %s, (1 << uint(l - i)): %d.\n", binary, l, result, i, string(c), (1 << uint(l - i)))
+			result += (1 << uint(l - i))
+		case '0':
+			continue
+		default:
+			return 0, fmt.Errorf("Non-binary character '%s' encountered.", string(c))
+		}
+	}
+
+	return result, nil
+}
