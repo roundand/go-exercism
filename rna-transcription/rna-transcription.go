44d7282e0fef7a9a16371eb60bb6d39a9f461d2b
Roundand <francis.norton@gmail.com>
2016-09-04 09:22:30 +0100

    Optimised for speed by remplacing map with switch.

--- rna-transcription/rna-transcription.go
+++ rna-transcription/rna-transcription.go
@@ -3,18 +3,25 @@ package strand
 
 const testVersion = 3
 
-var complement = map[rune]rune{
-	'G': 'C',
-	'C': 'G',
-	'T': 'A',
-	'A': 'U',
+func complement(r rune) rune {
+	switch r {
+	case 'G':
+		return 'C'
+	case 'C':
+		return 'G'
+	case 'T':
+		return 'A'
+	case 'A':
+		return 'U'
+	}
+	return 0 // consistent with behaviour of map version
 }
 
 // ToRNA returns a transcription of the supplied RNA string.
 func ToRNA(in string) string {
-	var ret []rune
-	for _, r := range in {
-		ret = append(ret, complement[r])
+	var ret = make([]rune, len(in))
+	for i, r := range in {
+		ret[i] = complement(r)
 	}
 	return string(ret)
 }
