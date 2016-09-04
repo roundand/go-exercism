af83453cae8f18936d9cde6a761f8b6f029d8b05
Roundand <francis.norton@gmail.com>
2016-09-05 00:52:24 +0100

    Solution to Exercism Roman-Numeral challenge.

--- roman-numerals/roman-numerals.go
+++ roman-numerals/roman-numerals.go
@@ -2,84 +2,53 @@
 package romannumerals
 
 import (
-  "fmt"
+	"fmt"
 )
 
 const (
 	testVersion = 2
-  MMMM        = 4000
-	M           = 1000
-  CM          = 900
-	D           = 500
-  CD          = 400
-	C           = 100
-  XC          = 90
-	L           = 50
-  XL          = 40
-	X           = 10
-  IX          = 9
-	V           = 5
-  IV          = 4
-	I           = 1
+	MMMM        = 4000
 )
+
+type value struct {
+	rom string
+	val int
+}
+
+var values = []value{
+	{"M", 1000},
+	{"CM", 900},
+	{"D", 500},
+	{"CD", 400},
+	{"C", 100},
+	{"XC", 90},
+	{"L", 50},
+	{"XL", 40},
+	{"X", 10},
+	{"IX", 9},
+	{"V", 5},
+	{"IV", 4},
+	{"I", 1},
+}
 
 // ToRomanNumeral converts an int number to a Roman numeral string.
 func ToRomanNumeral(num int) (string, error) {
-  if num >= MMMM {
-    return "", fmt.Errorf("number should be less than MMMM but was '%d'\n", num)
-  }
-	if num >= M {
-    r, _ := ToRomanNumeral(num - M)
-		return "M" + r, nil
+	if num >= MMMM {
+		return "", fmt.Errorf("number should be less than MMMM but was '%d'\n", num)
 	}
-  if num >= CM {
-    r, _ := ToRomanNumeral(num - CM)
-		return "CM" + r, nil
-	}
-	if num >= D {
-    r, _ := ToRomanNumeral(num - D)
-		return "D" + r, nil
-	}
-  if num >= CD {
-    r, _ := ToRomanNumeral(num - CD)
-		return "CD" + r, nil
-	}
-	if num >= C {
-    r, _ := ToRomanNumeral(num - C)
-		return "C" + r, nil
-	}
-  if num >= XC {
-    r, _ := ToRomanNumeral(num - XC)
-		return "XC" + r, nil
-	}
-	if num >= L {
-    r, _ := ToRomanNumeral(num - L)
-		return "L" + r, nil
-	}
-  if num >= XL {
-    r, _ := ToRomanNumeral(num - XL)
-		return "XL" + r, nil
-	}
-	if num >= X {
-    r, _ := ToRomanNumeral(num - X)
-		return "X" + r, nil
-	}
-  if num >= IX {
-		return "IX", nil
-	}
-	if num >= V {
-    r, _ := ToRomanNumeral(num - V)
-		return "V" + r, nil
-	}
-  if num >= IV {
-		return "IV", nil
-	}
-	if num > I {
-    r, _ := ToRomanNumeral(num - I)
-		return "I" + r, nil
-	}
-  if num == I {
-		return "I", nil
+	if num > 0 {
+		return roman(num, values), nil
 	}
 	return "", fmt.Errorf("can't convert value '%d'\n", num)
+}
+
+func roman(num int, vals []value) string {
+	switch {
+	case num == 0:
+		return ""
+	case num >= vals[0].val:
+		return vals[0].rom + roman(num-vals[0].val, vals)
+	default:
+		return roman(num, vals[1:])
+	}
 }
