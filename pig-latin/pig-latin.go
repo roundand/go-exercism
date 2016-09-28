d8db6d6119c22648bcb7e28f1a46840fff952f86
Roundand <francis.norton@gmail.com>
2016-09-28 07:52:33 +0100

    Passing solution to Exercism Pig Lating challenge.

--- pig-latin/pig-latin.go
+++ pig-latin/pig-latin.go
@@ -7,18 +7,10 @@ import "strings"
 func PigLatin(e string) string {
 	// Using channels for a flow-based solution.
 	in := make(chan string)
-	pl0 := make(chan string)
-	pl1 := make(chan string)
-	pl2 := make(chan string)
-	pl3 := make(chan string)
 	out := make(chan string)
 
 	go words(in, e)
-	go rules(pl0, pl1, pl2, pl3, in)
-	go code0(out, pl0)
-	go code1(out, pl1)
-	go code2(out, pl2)
-	go code3(out, pl3)
+	go rules(out, in)
 
 	// assemble output and return it
 	var ret []string
@@ -39,59 +31,48 @@ func isVowel(b byte) bool {
 	return b == 'a' || b == 'e' || b == 'i' || b == 'o' || b == 'u'
 }
 
-func rules(pl0, pl1, pl2, pl3, in chan string) {
-	defer close(pl0)
-	defer close(pl1)
-	defer close(pl2)
-	defer close(pl3)
+func rules(out, in chan string) {
+	defer close(out)
 	for w := range in {
 		switch {
 		case string(w[0:2]) == "ch":
-			pl2 <- w
+			out <- code2(w)
 		case string(w[0:2]) == "qu":
-			pl2 <- w
+			out <- code2(w)
 		case string(w[0:2]) == "sq":
-			pl3 <- w
+			out <- code3(w)
 		case string(w[0:2]) == "sc" && isVowel(w[2]):
-			pl2 <- w
+			out <- code2(w)
 		case string(w[0:2]) == "sc":
-			pl3 <- w
+			out <- code3(w)
 		case string(w[0:2]) == "th" && isVowel(w[2]):
-			pl2 <- w
+			out <- code2(w)
 		case string(w[0:2]) == "th":
-			pl3 <- w
+			out <- code3(w)
+		case string(w[0:1]) == "y" && !isVowel(w[1]):
+			out <- code0(w)
+		case string(w[0:1]) == "x" && !isVowel(w[1]):
+			out <- code0(w)
 		case isVowel(w[0]):
-			pl0 <- w
+			out <- code0(w)
 		default:
-			pl1 <- w
+			out <- code1(w)
 		}
 	}
 }
 
-func code0(out, pl chan string) {
-	defer close(out)
-	for w := range pl {
-		out <- w + "ay"
-	}
+func code0(w string) string {
+	return w + "ay"
 }
 
-func code1(out, pl chan string) {
-	//	defer close(out)
-	for w := range pl {
-		out <- w[1:] + w[0:1] + "ay"
-	}
+func code1(w string) string {
+	return w[1:] + w[0:1] + "ay"
 }
 
-func code2(out, pl chan string) {
-	//	defer close(out)
-	for w := range pl {
-		out <- w[2:] + w[0:2] + "ay"
-	}
+func code2(w string) string {
+	return w[2:] + w[0:2] + "ay"
 }
 
-func code3(out, pl chan string) {
-	//	defer close(out)
-	for w := range pl {
-		out <- w[3:] + w[0:3] + "ay"
-	}
+func code3(w string) string {
+	return w[3:] + w[0:3] + "ay"
 }
