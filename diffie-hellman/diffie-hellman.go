950485170b7ca44c3657c0108ce3eeaa66753356
Roundand <francis.norton@gmail.com>
2016-10-20 07:27:33 +0100

    Fixed error and tidied up.

--- diffie-hellman/diffie-hellman.go
+++ diffie-hellman/diffie-hellman.go
@@ -9,28 +9,25 @@ import (
 
 // using closures to provide cross-call state without need for vulnerable global variables.
 func uniqueRandomGen() func(*big.Int) *big.Int {
-	used := map[string]bool{big.NewInt(0).String(): true, big.NewInt(1).String(): true}
+	one := big.NewInt(1)
 	r := rand.New(rand.NewSource(9))
 	return func(max *big.Int) *big.Int {
 		var x = big.NewInt(0)
 		for n := 0; n < 1024; n++ {
 			x = x.Rand(r, max)
-			if !used[x.String()] {
-				// unique-ify any large numbers we return
-				if x.Cmp(big.NewInt(23)) != -1 {
-					used[x.String()] = true
-				}
+			// need a result greater than one
+			if x.Cmp(one) == 1 {
 				return x
 			}
 		}
-		fmt.Printf("used: %v/n", used)
 		panic(fmt.Sprintf("ran out of random numbers in range [2, %s)\n", max.String()))
 	}
 }
 
+// urg holds a seeded random number generator
 var urg = uniqueRandomGen()
 
-// PrivateKey returns a private key smaller than a limit.
+// PrivateKey returns a private key greater than 1 and smaller than p.
 func PrivateKey(p *big.Int) *big.Int {
 	return urg(p)
 }
@@ -46,15 +43,11 @@ func PublicKey(private, p *big.Int, g int64) *big.Int {
 func NewPair(p *big.Int, g int64) (private, public *big.Int) {
 	private = PrivateKey(p)
 	public = PublicKey(private, p, g)
-	fmt.Printf("p: %s\n", p.String())
-	fmt.Printf("g: %d\n", g)
-	fmt.Printf("private: %s\n", private.String())
-	fmt.Printf("public: %s\n\n", public.String())
 	return private, public
 }
 
 // SecretKey generates a secret key.
 func SecretKey(private1, public2, p *big.Int) *big.Int {
 	// Alice calculates secret key s, where s = B**a mod p
-	return public2.Exp(public2, private1, p)
+	return big.NewInt(0).Exp(public2, private1, p)
 }
