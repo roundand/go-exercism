782a48e7c41d4e4321830304f8936512e796558c
Roundand <francis.norton@gmail.com>
2016-11-20 18:02:40 +0000

    Refactored to exploit improved tokenisation.

--- wordy/wordy.go
+++ wordy/wordy.go
@@ -18,109 +18,62 @@ func Answer(e string) (int, bool) {
 	return start(t)
 }
 
-// an experiment in FSM using golang.
-// First pass, all hard-coded.
-
 // start
 // :what -> is
 func start(t []string) (int, bool) {
-	switch nextToken(t) {
-	case "WhatIs":
-		return whatIs(t[1:])
-	default:
-		return failed(fmt.Sprintf("Expected 'What' but got %q.\n", nextToken(t)))
+	tok, t := nextToken(t)
+	if tok == "WhatIs" {
+		return whatIs(t)
 	}
+	return failed(fmt.Sprintf("Expected 'What' but got %q.\n", tok))
 }
 
 // whatIs
 // :[n] (x := n)-> op
 func whatIs(t []string) (int, bool) {
-	s := nextToken(t)
+	s, t := nextToken(t)
 	i, err := strconv.Atoi(s)
 	if err != nil {
 		log.Printf("Could't parse %q as int.\n", s)
 		return failed(err.Error())
 	}
-	return op(i, t[1:])
+	return op(i, t)
 }
 
 // op
-// :(nil) -> end
-// :plus -> plus
-// :minus -> minus
-// :multiplied -> multipliedBy
-// :divided -> dividedBy
 func op(n int, t []string) (int, bool) {
-	switch nextToken(t) {
-	case "":
+	opTok, t := nextToken(t)
+	if opTok == "" { // we're all done here!
 		return n, true
+	}
+
+	// we expect each of our operators to be folloewd by a value
+	valTok, t := nextToken(t)
+	val, err := strconv.Atoi(valTok)
+	if err != nil {
+		return failed(err.Error())
+	}
+
+	switch opTok {
 	case "plus":
-		return plus(n, t[1:])
+		return op(n+val, t) //plus(n, t[1:])
 	case "minus":
-		return minus(n, t[1:])
+		return op(n-val, t)
 	case "multipliedBy":
-		return multipliedBy(n, t[1:])
+		return op(n*val, t)
 	case "dividedBy":
-		return dividedBy(n, t[1:])
+		return op(n/val, t)
 	default:
-		return failed(fmt.Sprintf("Expected <operator> but got %q.\n", nextToken(t)))
+		return failed(fmt.Sprintf("Expected <operator> but got %q.\n", opTok))
 	}
-}
-
-// plus
-// :[n] (x := x+n) -> op
-func plus(n int, t []string) (int, bool) {
-	s := nextToken(t)
-	i, err := strconv.Atoi(s)
-	if err != nil {
-		log.Printf("Could't parse %q as int.\n", s)
-		return failed(err.Error())
-	}
-	return op(n+i, t[1:])
-}
-
-// minus
-// :[n] (x := x-n) -> op
-func minus(n int, t []string) (int, bool) {
-	s := nextToken(t)
-	i, err := strconv.Atoi(s)
-	if err != nil {
-		log.Printf("Could't parse %q as int.\n", s)
-		return failed(err.Error())
-	}
-	return op(n-i, t[1:])
-}
-
-// multipliedBy
-// :[n] -> (x := x*n) -> op
-func multipliedBy(n int, t []string) (int, bool) {
-	s := nextToken(t)
-	i, err := strconv.Atoi(s)
-	if err != nil {
-		log.Printf("Could't parse %q as int.\n", s)
-		return failed(err.Error())
-	}
-	return op(n*i, t[1:])
-}
-
-// dividedBy
-// :n (x := x /n) -> op
-func dividedBy(n int, t []string) (int, bool) {
-	s := nextToken(t)
-	i, err := strconv.Atoi(s)
-	if err != nil {
-		log.Printf("Could't parse %q as int.\n", s)
-		return failed(err.Error())
-	}
-	return op(n/i, t[1:])
 }
 
 // returns next token, or empty string on end of input
-func nextToken(t []string) string {
+func nextToken(t []string) (string, []string) {
 	if len(t) == 0 {
-		return ""
+		return "", t
 	}
-	return t[0]
+	return t[0], t[1:]
 }
 
 // central spot for logging errors
