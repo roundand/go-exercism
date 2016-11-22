be66a4d75b8d9137da63148010664058c8775585
Roundand <francis.norton@gmail.com>
2016-11-22 07:57:41 +0000

    Refactored to run a state machine engine on a statemap data structure.

--- wordy/wordy.go
+++ wordy/wordy.go
@@ -8,63 +8,75 @@ import (
 	"strings"
 )
 
+type stateMap map[string]eventMap
+type eventMap map[string]move
+type move struct {
+	action    func(n, i int) int
+	nextState string
+}
+
+var sm = stateMap{
+	"start": {
+		"WhatIs": {func(n, i int) int { return i }, "op"},
+	},
+	"op": {
+		"plus":         {func(n, i int) int { return n + i }, "op"},
+		"minus":        {func(n, i int) int { return n - i }, "op"},
+		"multipliedBy": {func(n, i int) int { return n * i }, "op"},
+		"dividedBy":    {func(n, i int) int { return n / i }, "op"},
+		"?":            {func(n, i int) int { return n }, "end"},
+	},
+}
+
 // Answer accepts a prose arithmetic expression and returns an integer solution, or failure.
 func Answer(e string) (int, bool) {
-	e = strings.Replace(e, "?", "", -1)                         // question marks can be removed.
-	e = strings.Replace(e, "What is", "WhatIs", -1)             // Simplify What is prose
-	e = strings.Replace(e, "multiplied by", "multipliedBy", -1) // Simplify multiplication prose
-	e = strings.Replace(e, "divided by", "dividedBy", -1)       // Simplify division prose
-	t := strings.Split(e, " ")                                  // tokenise input.
-	return start(t)
+	e = strings.Replace(e, "?", " ? 0", -1) // question mark is final token, with dummy value.
+	e = strings.Replace(e, " is", "Is", -1) // Simplify "What is" text
+	e = strings.Replace(e, " by", "By", -1) // Simplify "multiplied by" and "divided by" text
+	t := strings.Split(e, " ")              // tokenise input.
+	opVals := opValStreamer(t)              // initialise event feeder with tokens
+	return sm.run("start", 0, opVals)
 }
 
-// start
-// :what -> is
-func start(t []string) (int, bool) {
-	tok, t := nextToken(t)
-	if tok == "WhatIs" {
-		return whatIs(t)
-	}
-	return failed(fmt.Sprintf("Expected 'What' but got %q.\n", tok))
-}
-
-// whatIs
-// :[n] (x := n)-> op
-func whatIs(t []string) (int, bool) {
-	s, t := nextToken(t)
-	i, err := strconv.Atoi(s)
-	if err != nil {
-		log.Printf("Could't parse %q as int.\n", s)
-		return failed(err.Error())
-	}
-	return op(i, t)
-}
-
-// op
-func op(n int, t []string) (int, bool) {
-	opTok, t := nextToken(t)
-	if opTok == "" { // we're all done here!
+// fsm engine
+func (sm stateMap) run(st string, n int, opVals func() (string, int, error)) (int, bool) {
+	if st == "end" {
 		return n, true
 	}
-
-	// we expect each of our operators to be folloewd by a value
-	valTok, t := nextToken(t)
-	val, err := strconv.Atoi(valTok)
+	op, val, err := opVals()
 	if err != nil {
-		return failed(err.Error())
+		return failed(fmt.Sprintf("couldn't parse val %q for op %q", val, op))
+	}
+	m, found := sm[st][op]
+	if !found {
+		return failed(fmt.Sprintf("Couldn't find event %q for state %q", op, st))
 	}
 
-	switch opTok {
-	case "plus":
-		return op(n+val, t) //plus(n, t[1:])
-	case "minus":
-		return op(n-val, t)
-	case "multipliedBy":
-		return op(n*val, t)
-	case "dividedBy":
-		return op(n/val, t)
-	default:
-		return failed(fmt.Sprintf("Expected <operator> but got %q.\n", opTok))
+	return sm.run(m.nextState, m.action(n, val), opVals)
+}
+
+// returns operator and optional value, or parse Int error
+func opValStreamer(tokens []string) func() (string, int, error) {
+	t := tokens // capture tokens as closure
+	return func() (string, int, error) {
+		opTok, t1 := nextToken(t)
+		t = t1
+		if opTok == "" {
+			return "", 0, nil
+		}
+
+		valTok, t2 := nextToken(t)
+		t = t2
+		if valTok == "" {
+			return opTok, 0, nil
+		}
+
+		val, err := strconv.Atoi(valTok)
+		if err != nil {
+			return "", 0, err
+		}
+
+		return opTok, val, nil
 	}
 }
 
