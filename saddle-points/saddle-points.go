254fbe2a13005326091a413892b65a66c91b367a
Roundand <francis.norton@gmail.com>
2016-12-18 12:07:23 +0000

    Solution for Exercism saddle-point exercis, unoptimised.

--- saddle-points/saddle-points.go
+++ saddle-points/saddle-points.go
@@ -48,10 +48,55 @@ func New(s string) (*Matrix, error) {
 	return &m, nil
 }
 
+// Col returns one column of a matrix
+func (m *Matrix) Col(i int) []int {
+	col := make([]int, len(*m))
+	for x := range *m {
+		col[x] = (*m)[x][i]
+	}
+	return col
+}
+
 // Pair is an array - not a slice - of two integers.
 type Pair [2]int
 
 // Saddle returns a list of all if any saddle points in a matrix.
 func (m *Matrix) Saddle() []Pair {
-	return []Pair{{0, 0}}
+	points := []Pair{}
+
+	// for every row in matrix ...
+	for r, row := range *m {
+
+		// ... for each element in row ...
+		for c, val := range row {
+
+			// ... if it's a saddle point, note it.
+			if val == highest(row) && val == lowest(m.Col(c)) {
+				points = append(points, Pair{r, c})
+			}
+		}
+	}
+
+	return points
+}
+
+// func highest returns highest value in an integer slice
+func highest(seq []int) int {
+	return most(seq, func(x, y int) bool { return x > y })
+}
+
+// func lowest returns lowest value in an integer slice
+func lowest(seq []int) int {
+	return most(seq, func(x, y int) bool { return x < y })
+}
+
+// most returns most more() value in an integer slice
+func most(seq []int, more func(x, y int) bool) int {
+	m := seq[0]
+	for _, v := range seq {
+		if more(v, m) {
+			m = v
+		}
+	}
+	return m
 }
